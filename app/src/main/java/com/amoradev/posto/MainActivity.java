f4a199fbb55176b4d4102f2d98c6fc752bb4d0ea
pablolucas890 <pablolucas890@gmail.com>
2023-08-01 10:34:52 -0300

    Open google and facebook on external browser

--- app/src/main/java/com/amoradev/posto/MainActivity.java
+++ app/src/main/java/com/amoradev/posto/MainActivity.java
@@ -2,14 +2,24 @@ package com.amoradev.posto;
 
 import static android.net.Uri.decode;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -21,22 +31,10 @@ import com.amoradev.posto.R;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            //super.onBackPressed();
-            webView.loadUrl("https://www.posto.amoradev.com.br/acesso");
-        }
-    }
-    private void enableHTML5AppCache(WebSettings settings) {
-        settings.setDomStorageEnabled(true);
-//        settings.setAppCachePath(getCacheDir().getAbsolutePath());
-//        settings.setAppCacheEnabled(true);
-        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-    }
+    private String userAgent;
+    private WebView webViewPopUp;
+    private Context globalContext;
+    private AlertDialog builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,34 +47,112 @@ public class MainActivity extends AppCompatActivity {
         WebSettings settings = webView.getSettings();
 
         settings.setUseWideViewPort(true);
+
+        // Set User Agent
+        userAgent = System.getProperty("http.agent");
+        settings.setUserAgentString(userAgent + "yourAppName");
+
+        // Enable Cookies
+        CookieManager.getInstance().setAcceptCookie(true);
+        if(android.os.Build.VERSION.SDK_INT >= 21)
+            CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+
+        // Handle Popups
+        webView.setWebChromeClient(new CustomChromeClient());
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setSupportMultipleWindows(true);
+        globalContext = this.getApplicationContext();
+
+        // WebView Tweaks
+        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        // settings.setAppCacheEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        settings.setUseWideViewPort(true);
+        settings.setSaveFormData(true);
+        settings.setEnableSmoothTransition(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
         //enableHTML5AppCache(settings);
         webView.loadUrl("https://www.posto.amoradev.com.br/acesso");
 
         webView.setWebViewClient(new WebViewClient() {
-
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
             public void onPageFinished(WebView view, String url) {
                 findViewById(R.id.splash).setVisibility(View.GONE);
                 findViewById(R.id.webview).setVisibility(View.VISIBLE);
             }
         });
+    }
 
-//       FirebaseMessaging.getInstance().getToken()
-//               .addOnCompleteListener(new OnCompleteListener<String>() {
-//                   @Override
-//                   public void onComplete(@NonNull Task<String> task) {
-//                       if (!task.isSuccessful()) {
-//                           Log.w("koike", "Fetching FCM registration token failed", task.getException());
-//                           return;
-//                       }
-//
-//                       // Get new FCM registration token
-//                       String token = task.getResult();
-//                       // czovt8_KTYyIk3Mef75e0n:APA91bFRtKOspG0NF_bI8o0KmewNiPUMvIK67TXHD12JPb8ui9vJFz32718fipnC_w8QZB8yZGXw_cxwf6MgWznUJiMI2EdRgE1vibluRC655xNkOFx_vtwhSswYCuBfb-AQkxCiWklO
-//                       // Log and toast
-//                       //String msg = getString(R.string.msg_token_fmt, token);
-//                       Log.d("koike", token);
-//                       //Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
-//                   }
-//               });
+    class CustomChromeClient extends WebChromeClient {
+
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog,
+                                      boolean isUserGesture, Message resultMsg) {
+            webViewPopUp = new WebView(globalContext);
+            webViewPopUp.setVerticalScrollBarEnabled(false);
+            webViewPopUp.setHorizontalScrollBarEnabled(false);
+            webViewPopUp.setWebChromeClient(new CustomChromeClient());
+            webViewPopUp.getSettings().setJavaScriptEnabled(true);
+            webViewPopUp.getSettings().setSaveFormData(true);
+            webViewPopUp.getSettings().setEnableSmoothTransition(true);
+            webViewPopUp.getSettings().setUserAgentString(userAgent + "yourAppName");
+
+            // pop the  webview with alert dialog
+            builder = new AlertDialog.Builder(MainActivity.this).create();
+            builder.setTitle("");
+            builder.setView(webViewPopUp);
+            builder.setButton(DialogInterface.BUTTON_NEGATIVE, "Fechar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    webViewPopUp.destroy();
+                    dialog.dismiss();
+                }
+            });
+            builder.show();
+            builder.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptCookie(true);
+            if(android.os.Build.VERSION.SDK_INT >= 21) {
+                cookieManager.setAcceptThirdPartyCookies(webViewPopUp, true);
+                cookieManager.setAcceptThirdPartyCookies(webView, true);
+            }
+
+            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+            transport.setWebView(webViewPopUp);
+            resultMsg.sendToTarget();
+
+            return true;
+        }
+
+        @Override
+        public void onCloseWindow(WebView window) {
+            try {
+                webViewPopUp.destroy();
+            } catch (Exception e) {
+                Log.d("Destroyed with Error ", e.getStackTrace().toString());
+            }
+
+            try {
+                builder.dismiss();
+            } catch (Exception e) {
+                Log.d("Dismissed with Error: ", e.getStackTrace().toString());
+            }
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            webView.loadUrl("https://www.posto.amoradev.com.br/acesso");
+        }
     }
 }
